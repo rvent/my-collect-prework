119d2ae3f03d7d36a9cb800c0513333f23058413
Rene Ventura <venturar729@gmail.com>
2018-12-21 19:31:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(collection)
   i = 0
   new_arr = []
   while i < collection.length
-    new_arr << yield collection[i]
+    yield collection[i]
+    new_arr << collection[i]
     i += 1
   end
   new_arr
